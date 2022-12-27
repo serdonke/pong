d9bce1a5bffbb6928967fa43633be3db00e780bf
Prateek Shukla <keetarpalkuhs@gmail.com>
2022-12-27 13:19:35 +0530

    Implement base game

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # Build and run the file
 
 function build() {
-    gcc pong.c -o ./build/pong -I. -I../raylib/src -L. -L../raylib/src -lraylib -lGL -lc -lm -lpthread -ldl -lrt -lX11 && ./build/pong
+    gcc pong.c -o ./build/pong -Wall -I. -I../raylib/src -L. -L../raylib/src -lraylib -lGL -lc -lm -lpthread -ldl -lrt -lX11 && ./build/pong
 }
 
 if [ -d ./build ]; then
--- pong.c
+++ pong.c
@@ -8,15 +8,26 @@ typedef struct Paddle
     Color color;
 } Paddle;
 
+typedef struct Ball
+{
+    Vector2 position;
+    float radius;
+    Color color;
+} Ball;
+
 #define PADDLE_HEIGHT 140
 #define PADDLE_WIDTH  18
-#define PADDLE_SPEED  500
+#define PADDLE_SPEED  800
+
+#define BALL_RADIUS 5
+#define BALL_SPEED  600
+#define BALL_OFFSET 400
 
 int main(void)
 {
     int monitor;
-    int height  = 720;
-    int width   = 1280;
+    int height = 720;
+    int width  = 1280;
 
     InitWindow(width, height, "Pong");
 
@@ -25,17 +36,26 @@ int main(void)
     playerPaddle.entity.height = AIPaddle.entity.height = PADDLE_HEIGHT;
     playerPaddle.entity.width  = AIPaddle.entity.width  = PADDLE_WIDTH;
     playerPaddle.entity.x = 20;
-    playerPaddle.entity.y = height / 2;
+    playerPaddle.entity.y = GetRenderHeight() / 2;
     AIPaddle.entity.x = GetRenderWidth() - 40;
-    AIPaddle.entity.y = 500;
+    AIPaddle.entity.y = GetRenderHeight() / 2;
+
+    Ball ball;
+    ball.position.x = GetRenderWidth() / 2;
+    ball.position.y = GetRenderHeight() / 2;
+    ball.radius = BALL_RADIUS;
+    ball.color  = RAYWHITE;
+    int direction = GetRandomValue(-1, 1);
+    float offset  = GetRandomValue(-BALL_OFFSET, BALL_OFFSET);
 
     monitor = GetCurrentMonitor();
-    printf("Monitor: %d\nHeight: %d\n Width: %d\n",monitor, GetRenderHeight(), GetRenderWidth());
+    printf("Monitor: %d\nHeight: %d\nWidth: %d\n",monitor, GetRenderHeight(), GetRenderWidth());
+
     SetTargetFPS(GetMonitorRefreshRate(monitor));
 
     while (!WindowShouldClose())
     {
-        if (IsKeyDown(KEY_S) && playerPaddle.entity.y < GetRenderHeight() - PADDLE_HEIGHT)
+        if(IsKeyDown(KEY_S) && playerPaddle.entity.y < GetRenderHeight() - PADDLE_HEIGHT)
         {
             playerPaddle.entity.y += PADDLE_SPEED * GetFrameTime();
         }
@@ -43,11 +63,46 @@ int main(void)
         {
             playerPaddle.entity.y -= PADDLE_SPEED * GetFrameTime();
         }
-        
+        //AIPAddle Test
+        if(IsKeyDown(KEY_DOWN) && AIPaddle.entity.y < GetRenderHeight() - PADDLE_HEIGHT)
+        {
+            AIPaddle.entity.y += PADDLE_SPEED * GetFrameTime();
+        }
+        else if(IsKeyDown(KEY_UP) && AIPaddle.entity.y > 0)
+        {
+            AIPaddle.entity.y -= PADDLE_SPEED * GetFrameTime();
+        }
+
+
+        //Ball Movement
+        ball.position.x += (BALL_SPEED * (direction ? -1 : 1)) * GetFrameTime();
+        ball.position.y += offset * GetFrameTime();
+//        if(ball.position.x <= 0) { direction = 0; }
+//        else if(ball.position.x >= GetRenderWidth()) { direction = -1; }
+        //Ball environment behaviour
+        if(ball.position.x <= 0 || ball.position.x >= GetRenderWidth())
+        {
+            ball.position.x = GetRenderWidth() / 2;
+            ball.position.y = GetRenderHeight() / 2;
+            direction = GetRandomValue(-1, 1);
+            offset = GetRandomValue(-BALL_OFFSET, BALL_OFFSET);            
+        }
+        if(ball.position.y <= 0 + ball.radius || ball.position.y >= GetRenderHeight() - ball.radius)
+        {    
+            offset *= -1;
+        }
+        //Ball-player Interaction
+        if(CheckCollisionCircleRec(ball.position, ball.radius, ((ball.position.x <= width / 2) ? playerPaddle.entity : AIPaddle.entity)))
+        {
+            direction = ball.position.x <= width / 2 ? 0 : 1;
+            offset = GetRandomValue(-600, 600);
+        }
+
         BeginDrawing();
             ClearBackground(BLACK);
             DrawRectangleRec(playerPaddle.entity, playerPaddle.color);
             DrawRectangleRec(AIPaddle.entity, AIPaddle.color);
+            DrawCircleV(ball.position, ball.radius, ball.color);
         EndDrawing();
     }
     
