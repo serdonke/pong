2deceb778b93f4027b537e62e2faacf0d356f021
Prateek Shukla <keetarpalkuhs@gmail.com>
2022-12-27 10:42:05 +0530

    Implement paddle movement and clamp movement

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Build and run the file
+
+function build() {
+    gcc pong.c -o ./build/pong -I. -I../raylib/src -L. -L../raylib/src -lraylib -lGL -lc -lm -lpthread -ldl -lrt -lX11 && ./build/pong
+}
+
+if [ -d ./build ]; then
+    build
+else
+    mkdir build && build
+fi
--- pong.c
+++ pong.c
@@ -10,33 +10,38 @@ typedef struct Paddle
 
 #define PADDLE_HEIGHT 140
 #define PADDLE_WIDTH  18
+#define PADDLE_SPEED  500
 
 int main(void)
 {
-    int monitor = GetCurrentMonitor();
-    int height  = GetMonitorHeight(monitor);
-    int width   = GetMonitorWidth(monitor);
+    int monitor;
+    int height  = 720;
+    int width   = 1280;
 
+    InitWindow(width, height, "Pong");
 
     Paddle playerPaddle, AIPaddle;
     playerPaddle.color = AIPaddle.color = RAYWHITE;
     playerPaddle.entity.height = AIPaddle.entity.height = PADDLE_HEIGHT;
     playerPaddle.entity.width  = AIPaddle.entity.width  = PADDLE_WIDTH;
-    playerPaddle.entity.x = width + 20;
-    playerPaddle.entity.y = 500;
-    AIPaddle.entity.x = GetRenderWidth() - 50;
+    playerPaddle.entity.x = 20;
+    playerPaddle.entity.y = height / 2;
+    AIPaddle.entity.x = GetRenderWidth() - 40;
     AIPaddle.entity.y = 500;
 
-    InitWindow(width, height, "Pong");
+    monitor = GetCurrentMonitor();
     printf("Monitor: %d\nHeight: %d\n Width: %d\n",monitor, GetRenderHeight(), GetRenderWidth());
-
     SetTargetFPS(GetMonitorRefreshRate(monitor));
 
     while (!WindowShouldClose())
     {
-        if (IsKeyDown(KEY_DOWN))
+        if (IsKeyDown(KEY_S) && playerPaddle.entity.y < GetRenderHeight() - PADDLE_HEIGHT)
         {
-            playerPaddle.entity.y += 300 * GetFrameTime();
+            playerPaddle.entity.y += PADDLE_SPEED * GetFrameTime();
+        }
+        else if(IsKeyDown(KEY_W) && playerPaddle.entity.y > 0)
+        {
+            playerPaddle.entity.y -= PADDLE_SPEED * GetFrameTime();
         }
         
         BeginDrawing();
