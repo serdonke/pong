1f57c2dfd44360737332412497b7b7e91feaa822
Prateek Shukla <keetarpalkuhs@gmail.com>
2023-01-05 14:38:07 +0530

    Add basic state-management

--- pong.c
+++ pong.c
@@ -2,6 +2,8 @@
 #include <raymath.h> 
 #include <stdio.h>
 
+void stateSwitch();
+
 typedef struct Paddle
 {
     Rectangle entity;
@@ -23,6 +25,12 @@ typedef struct Ball
 #define BALL_RADIUS 5
 #define BALL_SPEED  600
 #define BALL_OFFSET 600
+
+int GAMESTATE = 0;
+// 0 -> menu
+// 1 -> play
+// 2 -> paus
+// 3 -> over
 
 int main(void)
 {
@@ -46,13 +54,13 @@ int main(void)
     playerPaddle.entity.height = AIPaddle.entity.height = PADDLE_HEIGHT;
     playerPaddle.entity.width  = AIPaddle.entity.width  = PADDLE_WIDTH;
     playerPaddle.entity.x = 20;
-    playerPaddle.entity.y = GetRenderHeight() / 2;
+    playerPaddle.entity.y = GetRenderHeight() / 2.0;
     AIPaddle.entity.x = GetRenderWidth() - 40;
-    AIPaddle.entity.y = GetRenderHeight() / 2;
+    AIPaddle.entity.y = GetRenderHeight() / 2.0;
 
     Ball ball;
-    ball.position.x = GetRenderWidth() / 2;
-    ball.position.y = GetRenderHeight() / 2;
+    ball.position.x = GetRenderWidth() / 2.0;
+    ball.position.y = GetRenderHeight() / 2.0;
     ball.radius = BALL_RADIUS;
     ball.color  = RAYWHITE;
     int direction = GetRandomValue(-1, 1);
@@ -71,98 +79,131 @@ int main(void)
     SetTargetFPS(GetMonitorRefreshRate(monitor));
     while (!WindowShouldClose())
     {
-        if(IsKeyDown(KEY_S) && playerPaddle.entity.y < GetRenderHeight() - PADDLE_HEIGHT)
+        stateSwitch();
+        switch (GAMESTATE)
         {
-            playerPaddle.entity.y += PADDLE_SPEED * GetFrameTime();
-        }
-        else if(IsKeyDown(KEY_W) && playerPaddle.entity.y > 0)
-        {
-            playerPaddle.entity.y -= PADDLE_SPEED * GetFrameTime();
-        }
-        // //AIPAddle Test
-        // if(IsKeyDown(KEY_DOWN) && AIPaddle.entity.y < GetRenderHeight() - PADDLE_HEIGHT)
-        // {
-        //     AIPaddle.entity.y += PADDLE_SPEED * GetFrameTime();
-        // }
-        // else if(IsKeyDown(KEY_UP) && AIPaddle.entity.y > 0)
-        // {
-        //     AIPaddle.entity.y -= PADDLE_SPEED * GetFrameTime();
-        // }
-        //AI Paddle
-        if(ball.position.x < GetRenderWidth() / 2.0)
-        {
-            if (AIPaddle.entity.y > GetRenderHeight() / 2.0 - PADDLE_HEIGHT / 2)
-            {
-                AIPaddle.entity.y -= (PADDLE_SPEED * GetFrameTime());
-            }
-            else 
-            {
-                AIPaddle.entity.y += (PADDLE_SPEED * GetFrameTime());    
-            }
-        }
-        else if (ball.position.x > GetRenderWidth() / 2.0)
-        {
-            if (AIPaddle.entity.y != ball.position.y)
-            {
-                if (AIPaddle.entity.y > ball.position.y)
+            case 0:
+                if(IsKeyDown(KEY_S) && playerPaddle.entity.y < GetRenderHeight() - PADDLE_HEIGHT)
+                {
+                    playerPaddle.entity.y += PADDLE_SPEED * GetFrameTime();
+                }
+                else if(IsKeyDown(KEY_W) && playerPaddle.entity.y > 0)
+                {
+                    playerPaddle.entity.y -= PADDLE_SPEED * GetFrameTime();
+                }
+                //AIPAddle Test
+                if(IsKeyDown(KEY_DOWN) && AIPaddle.entity.y < GetRenderHeight() - PADDLE_HEIGHT)
+                {
+                    AIPaddle.entity.y += PADDLE_SPEED * GetFrameTime();
+                }
+                else if(IsKeyDown(KEY_UP) && AIPaddle.entity.y > 0)
                 {
                     AIPaddle.entity.y -= PADDLE_SPEED * GetFrameTime();
                 }
-                else
-                {
-                    if(AIPaddle.entity.y < GetRenderHeight() - PADDLE_HEIGHT)
-                        AIPaddle.entity.y += PADDLE_SPEED * GetFrameTime();
-                }
-            }
-        }
+            break;
 
-        //Ball Movement
-        ball.position.x += (BALL_SPEED * (direction ? -1 : 1)) * GetFrameTime();
-        ball.position.y += offset * GetFrameTime();
-//        if(ball.position.x <= 0) { direction = 0; }
-//        else if(ball.position.x >= GetRenderWidth()) { direction = -1; }
-        //Ball environment behaviour
-        if(ball.position.x <= 0 || ball.position.x >= GetRenderWidth())
-        {
-            ball.position.x = GetRenderWidth() / 2;
-            ball.position.y = GetRenderHeight() / 2;
-            direction = GetRandomValue(-1, 1);
-            offset = GetRandomValue(-BALL_OFFSET, BALL_OFFSET);
-            PlaySound(score);
-        }
-        if(ball.position.y <= 0 + ball.radius || ball.position.y >= GetRenderHeight() - ball.radius)
-        {    
-            offset *= -1;
-            PlaySound(wallHit);            
-        }
-        //Ball-player Interaction
-        if(CheckCollisionCircleRec(ball.position, ball.radius, ((ball.position.x <= width / 2) ? playerPaddle.entity : AIPaddle.entity)))
-        {
-            PlaySound(impact);
-            direction = ball.position.x <= width / 2 ? 0 : 1;
-            offset = GetRandomValue(-BALL_OFFSET, BALL_OFFSET);
-            PlaySound(impact);
-        }
-        //Track scores
-        if(CheckCollisionCircleRec(ball.position, ball.radius, playerPaddle.entity))
-        {
-            playerPaddle.score += 1;
-            sprintf(strplayerScore, "%d", playerPaddle.score);
-        }
-        if(CheckCollisionCircleRec(ball.position, ball.radius, AIPaddle.entity))
-        {
-            AIPaddle.score += 1;
-            sprintf(strAIScore, "%d", AIPaddle.score);
+            case 1: // Play State
+                if(IsKeyDown(KEY_S) && playerPaddle.entity.y < GetRenderHeight() - PADDLE_HEIGHT)
+                {
+                    playerPaddle.entity.y += PADDLE_SPEED * GetFrameTime();
+                }
+                else if(IsKeyDown(KEY_W) && playerPaddle.entity.y > 0)
+                {
+                    playerPaddle.entity.y -= PADDLE_SPEED * GetFrameTime();
+                }
+                // //AIPAddle Test
+                // if(IsKeyDown(KEY_DOWN) && AIPaddle.entity.y < GetRenderHeight() - PADDLE_HEIGHT)
+                // {
+                //     AIPaddle.entity.y += PADDLE_SPEED * GetFrameTime();
+                // }
+                // else if(IsKeyDown(KEY_UP) && AIPaddle.entity.y > 0)
+                // {
+                //     AIPaddle.entity.y -= PADDLE_SPEED * GetFrameTime();
+                // }
+                //AI Paddle
+                if(ball.position.x < GetRenderWidth() / 2.0)
+                {
+                    if (AIPaddle.entity.y > GetRenderHeight() / 2.0 - PADDLE_HEIGHT / 2.0)
+                    {
+                        AIPaddle.entity.y -= (PADDLE_SPEED * GetFrameTime());
+                    }
+                    else 
+                    {
+                        AIPaddle.entity.y += (PADDLE_SPEED * GetFrameTime());    
+                    }
+                }
+                else if (ball.position.x > GetRenderWidth() / 2.0)
+                {
+                    if (AIPaddle.entity.y != ball.position.y)
+                    {
+                        if (AIPaddle.entity.y > ball.position.y)
+                        {
+                            AIPaddle.entity.y -= PADDLE_SPEED * GetFrameTime();
+                        }
+                        else
+                        {
+                            if(AIPaddle.entity.y < GetRenderHeight() - PADDLE_HEIGHT)
+                                AIPaddle.entity.y += PADDLE_SPEED * GetFrameTime();
+                        }
+                    }
+                }
+
+                //Ball Movement
+                ball.position.x += (BALL_SPEED * (direction ? -1 : 1)) * GetFrameTime();
+                ball.position.y += offset * GetFrameTime();
+
+                if(ball.position.y <= 0 + ball.radius || ball.position.y >= GetRenderHeight() - ball.radius)
+                {    
+                    offset *= -1;
+                    PlaySound(wallHit);            
+                }
+                //Ball-player Interaction
+                if(CheckCollisionCircleRec(ball.position, ball.radius, ((ball.position.x <= width / 2.0) ? playerPaddle.entity : AIPaddle.entity)))
+                {
+                    direction = ball.position.x <= width / 2.0 ? 0 : 1;
+                    offset = GetRandomValue(-BALL_OFFSET, BALL_OFFSET);
+                    PlaySound(impact);
+                }
+                //Track balls and scores
+                if(ball.position.x <= 0)
+                {
+                    ball.position.x = GetRenderWidth() / 2.0;
+                    ball.position.y = GetRenderHeight() / 2.0;
+                    direction = GetRandomValue(-1, 1);
+                    offset = GetRandomValue(-BALL_OFFSET, BALL_OFFSET);
+                    PlaySound(score);
+                    playerPaddle.score += 1;
+                    sprintf(strplayerScore, "%d", playerPaddle.score);
+                }
+                if(ball.position.x >= GetRenderWidth())
+                {
+                    ball.position.x = GetRenderWidth() / 2.0;
+                    ball.position.y = GetRenderHeight() / 2.0;
+                    direction = GetRandomValue(-1, 1);
+                    offset = GetRandomValue(-BALL_OFFSET, BALL_OFFSET);
+                    PlaySound(score);
+                    AIPaddle.score += 1;
+                    sprintf(strAIScore, "%d", AIPaddle.score);
+                }
+            break;
         }
 
 
         BeginDrawing();
+            switch (GAMESTATE)
+            {
+                case 0:
+                break;
+
+                case 1:
+                    DrawCircleV(ball.position, ball.radius, ball.color);
+                break;
+            }
+            DrawText(strplayerScore, GetRenderWidth() / 2 - 200, GetRenderHeight() / 2 - 200, 30, RAYWHITE);
             ClearBackground(BLACK);
+            DrawText(strAIScore, GetRenderWidth() / 2 + 200, GetRenderHeight() / 2 - 200, 30, RAYWHITE);
             DrawRectangleRec(playerPaddle.entity, playerPaddle.color);
             DrawRectangleRec(AIPaddle.entity, AIPaddle.color);
-            DrawCircleV(ball.position, ball.radius, ball.color);
-            DrawText(strplayerScore, GetRenderWidth() / 2 - 200, GetRenderHeight() / 2 - 200, 30, RAYWHITE);
-            DrawText(strAIScore, GetRenderWidth() / 2 + 200, GetRenderHeight() / 2 - 200, 30, RAYWHITE);
         EndDrawing();
     }
     
@@ -171,4 +212,12 @@ int main(void)
     MemFree(strAIScore);
     CloseAudioDevice();
     CloseWindow();
+}
+
+void stateSwitch()
+{
+    if (IsKeyPressed(KEY_ENTER))
+    { 
+        GAMESTATE = (GAMESTATE) ? 0 : 1;
+    }
 }
